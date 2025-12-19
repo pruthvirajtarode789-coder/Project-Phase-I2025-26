75c9a71374f076d787a6a5c56917418497713179
Shrikrushna Gandhewar <162042680+Krishna9422@users.noreply.github.com>
2025-12-19 12:11:55 +0530

    updated

---
+++ Shrikrushna_214_BtechB_AI_DRIVEN_SMART_SEEDING/Project/frontend/src/pages/Index.tsx
@@ -0,0 +1,17 @@
+import Hero from "@/components/Hero";
+import MainFeatures from "@/components/MainFeatures";
+import LiveSections from "@/components/LiveSections";
+import DetailedFeatures from "@/components/DetailedFeatures";
+
+const Index = () => {
+  return (
+    <div>
+      <Hero />
+      <MainFeatures />
+      <LiveSections />
+      <DetailedFeatures />
+    </div>
+  );
+};
+
+export default Index;
